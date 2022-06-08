37481c5f5a4ee4b8d1395cc6fe07cce5be10b4ef
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-06-08 10:12:47 -0400

    Added example

--- Feedback/ViewController.swift
+++ Feedback/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import Algorithms
 
 class ViewController: UIViewController {
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
 
+    enum Section: Hashable {
+        case main(id: UUID)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureHierarchy()
+        configureDataSource()
+        applyInitialSnapshot()
     }
-
-
+    
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25),
+                                               heightDimension: .fractionalHeight(0.25))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+    }
+    
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Int> { (cell, indexPath, int) in
+            var content = cell.defaultContentConfiguration()
+            content.text = int.description
+            content.textProperties.font = .preferredFont(forTextStyle: .title1)
+            cell.contentConfiguration = content
+            
+            var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
+            backgroundConfig.backgroundColor = .systemBlue.withAlphaComponent(0.25)
+            cell.backgroundConfiguration = backgroundConfig
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: Int) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+        }
+    }
+    
+    func applyInitialSnapshot(){
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        
+        for chunk in Array(0..<80).chunks(ofCount: 10) {
+            let section = Section.main(id: UUID())
+            snapshot.appendSections([section])
+            snapshot.appendItems(Array(chunk), toSection: section)
+        }
+        
+        dataSource.apply(snapshot)
+    }
 }
-
